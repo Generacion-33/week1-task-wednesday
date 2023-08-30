48274a1910445758cacdd4ddc8be62228ef11da6
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-08-30 20:55:45 -0300

    advances

---
+++ src/task.js
@@ -0,0 +1,156 @@
+/*******************************/
+/* Ejercicios con operadores: */
+/******************************/
+// ¿Cual es el resultado de las siguiten expresiones?
+
+//1 + 2 + '3'
+const exercise1 = null
+
+//1 + 2 + '3' + 4
+const exercise2 = null
+
+//10 + 4 - 2 * 5
+const exercise3 = null
+
+//11 % 4
+const exercise4 = null
+
+//10 * 10
+const exercise5 = null
+
+
+// ¿Cuáles son los valores finales de todas las variables a, b, c y d después del código a continuación?
+
+let a = 1
+let b = 1
+
+const c = ++a
+const d = b++
+
+// cuanto vale a?
+const exercise6 = null
+
+// cuanto vale b?
+const exercise7 = null
+
+// cuanto vale c?
+const exercise8 = null
+
+// cuanto vale d?
+const exercise9 = null
+
+// ¿Cuáles son los valores de ‘e’ y ‘f’ después del código a continuación?
+
+let e = 2
+const f = 1 + (e *= 2)
+
+//valor de e
+const exercise10 = null
+
+//valor de f
+const exercise11 = null
+
+
+// ¿Cuál es el valor de ‘g’ después del código a continuación?
+let g = 3
+g += 5
+
+// valor de g
+const exercise12 = null
+
+
+// ¿Cuál es el valor de cada comparación después del código a continuación?
+
+// 5 > 4 
+const exercise13 = null
+
+//  'banco' < 'barco'
+const exercise14 = null
+
+//80 >= 15
+const exercise15 = null
+
+// 500 === 100 
+const exercise16 = null
+
+// 500 <= 100
+const exercise17 = null
+
+// 2 > 12
+const exercise18 = null
+
+// 0 === 1
+const exercise19 = null
+
+// 1 !== 0
+const exercise20 = null
+
+// undefined === null
+const exercise21 = null
+
+//null == ''
+const exercise22 = null
+
+//' ' === null
+const exercise23 = null
+
+// ¿Cuál es el resultado de cada valor después del código a continuación?
+
+//null || 2 || undefined
+const exercise24 = null
+
+//1 || 2 || 3
+const exercise25 = null
+
+//1 && null && 2
+const exercise26 = null
+
+//1 && 2
+const exercise27 = null
+
+//null || 2 && 3 || 4
+const exercise28 = null
+
+// null || 0 || 35
+const exercise29 = null
+
+//null && 0 && 35
+const exercise30 = null
+
+//null ?? 0 ?? 35
+const exercise31 = null
+
+
+module.exports = {
+  exercise1,
+  exercise2,
+  exercise3,
+  exercise4,
+  exercise5,
+  exercise6,
+  exercise7,
+  exercise8,
+  exercise9,
+  exercise10,
+  exercise11,
+  exercise12,
+  exercise13,
+  exercise14,
+  exercise15,
+  exercise16,
+  exercise17,
+  exercise18,
+  exercise19,
+  exercise20,
+  exercise21,
+  exercise22,
+  exercise23,
+  exercise24,
+  exercise25,
+  exercise26,
+  exercise27,
+  exercise28,
+  exercise29,
+  exercise30,
+  exercise31,
+}
---
+++ src/test/task.test.js
@@ -0,0 +1,125 @@
+const { exercise1, exercise2, exercise3, exercise4, exercise5, exercise6, exercise7, exercise8, exercise9, exercise10, exercise20, exercise19, exercise18, exercise17, exercise16, exercise15, exercise14, exercise13, exercise12, exercise11, exercise21, exercise22, exercise23, exercise24, exercise25, exercise26, exercise27, exercise28, exercise29, exercise30, exercise31 } = require('../task')
+
+test("exercise1", () => {
+  expect(exercise1).toBe(33)
+})
+
+test("exercise2", () => {
+  expect(exercise2).toBe('334')
+})
+
+test("exercise3", () => {
+  expect(exercise3).toBe(4)
+})
+
+test("exercise4", () => {
+  expect(exercise4).toBe(3)
+})
+
+test("exercise5", () => {
+  expect(exercise5).toBe(100)
+})
+
+test("exercise6", () => {
+  expect(exercise6).toBe(2)
+})
+
+test("exercise7", () => {
+  expect(exercise7).toBe(2)
+})
+
+test("exercise8", () => {
+  expect(exercise8).toBe(2)
+})
+
+test("exercise9", () => {
+  expect(exercise9).toBe(1)
+})
+
+test("exercise10", () => {
+  expect(exercise10).toBe(4)
+})
+
+test("exercise11", () => {
+  expect(exercise11).toBe(5)
+})
+
+test("exercise12", () => {
+  expect(exercise12).toBe(8)
+})
+
+test("exercise13", () => {
+  expect(exercise13).toBe(true)
+})
+
+test("exercise14", () => {
+  expect(exercise14).toBe(true)
+})
+
+test("exercise15", () => {
+  expect(exercise15).toBe(true)
+})
+
+test("exercise16", () => {
+  expect(exercise16).toBe(false)
+})
+
+test("exercise17", () => {
+  expect(exercise17).toBe(false)
+})
+
+test("exercise18", () => {
+  expect(exercise18).toBe(false)
+})
+
+test("exercise19", () => {
+  expect(exercise19).toBe(false)
+})
+
+test("exercise20", () => {
+  expect(exercise20).toBe(true)
+})
+
+test("exercise21", () => {
+  expect(exercise21).toBe(false)
+})
+
+test("exercise22", () => {
+  expect(exercise22).toBe(false)
+})
+
+test("exercise23", () => {
+  expect(exercise23).toBe(false)
+})
+
+test("exercise24", () => {
+  expect(exercise24).toBe(2)
+})
+
+test("exercise25", () => {
+  expect(exercise25).toBe(1)
+})
+
+test("exercise26", () => {
+  expect(exercise26).toBe(null)
+})
+
+test("exercise27", () => {
+  expect(exercise27).toBe(2)
+})
+
+test("exercise28", () => {
+  expect(exercise28).toBe(3)
+})
+
+test("exercise29", () => {
+  expect(exercise29).toBe(35)
+})
+
+test("exercise30", () => {
+  expect(exercise30).toBe(null)
+})
+
+test("exercise31", () => {
+  expect(exercise31).toBe(0)
+})
